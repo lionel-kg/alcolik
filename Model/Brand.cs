cc987abf500a20748fb9b6a7308ee07082f170c6
lionel-kg <lionel77350@gmail.com>
2022-11-03 11:28:04 +0100

    init project

---
+++ Model/Brand.cs
@@ -0,0 +1,11 @@
+﻿using alcolikLib.Model;
+
+namespace alcolik.Model
+{
+    public class Brand : BaseModel
+    {
+        public string Slogan { get; set; }
+
+        public IEnumerable<Alcool> Alcools { get; set; }
+    }
+}
